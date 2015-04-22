0880f1a2aa2a0a2b9237b8c7fdafaeab4f6b3a3c
王哲 <Hessen@outetsus-MacBook-Pro.local>
2015-04-22 19:48:50 +0800

    update dir name

---
+++ ApiServer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "K8APITransform/K8APITransform/ApiServer/docs"
+	_ "K8APITransform/K8APITransform/ApiServer/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
