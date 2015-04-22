26b12f8cbe9fea69f974666c0163aa9e6a2081e4
王哲 <Hessen@outetsus-MacBook-Pro.local>
2015-04-22 15:03:53 +0800

    add beego api project

---
+++ ApiTransform/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "testBee/ApiTransform/docs"
+	_ "testBee/ApiTransform/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
